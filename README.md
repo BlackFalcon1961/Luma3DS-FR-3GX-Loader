442203ebc3ff5b871373dc7fffbc6f9db234accf
BlackFalcon1961 <60007836+BlackFalcon1961@users.noreply.github.com>
2023-03-28 14:34:37 +0200

    main

---
+++ README.md
@@ -0,0 +1,40 @@
+# Luma3DS FR 3GX Loader
+*(N)3DS "Custom Firmware"*
+
+![](https://github.com/BlackFalcon1961/Luma3DS-FR-3GX-Loader/blob/images/Luma3DS-FR-300px.png)
+
+**ATTENTION : CE PROJET EST ACTUELLEMENT EN COURS DE TRADUCTION**
+
+### Qu'est-ce que c'est ?
+**Luma3DS** est un programme pour corriger le logiciel système des consoles portables (New) Nintendo 3DS/2DS "à la volée", ajoutant des fonctionnalités telles que les paramètres de langue par jeu, des capacités de débogage pour les développeurs et la suppression des restrictions appliquées par Nintendo telles que le verrouillage régional.
+
+Il vous permet également d'exécuter du contenu non autorisé ("homebrew") en supprimant les vérifications de signature.
+Pour l'utiliser, vous aurez besoin d'une console capable d'exécuter un logiciel homebrew sur le processeur Arm9
+
+Depuis la v8.0, Luma3DS a son propre menu en jeu, activable avec <kbd>L+Bas+Select</kbd> (regardez les [notes de mise à jour](https://github.com/LumaTeam/Luma3DS/releases/tag/v8.0)).
+
+#
+### Compilation :
+* Prérequis
+    1. git
+    2. [makerom](https://github.com/jakcron/Project_CTR) dans PATH
+    3. [firmtool](https://github.com/TuxSH/firmtool)
+    4. devkitARM+libctru à jour
+1. Clonez le dépôt avec `git clone https://github.com/LumaTeam/Luma3DS.git`
+2. Exécutez `make`.
+
+    Le `boot.firm` produit est destiné à être copié à la racine de votre carte SD pour une utilisation avec Boot9Strap.
+
+#
+### Installation / Utilisation / Fonctionnalités
+Consultez le [wiki de Luma3DS FR](https://github.com/BlackFalcon1961/Luma3DS-FR-3GX-Loader/wiki)
+
+#
+### Remerciements
+Regardez [ici](https://github.com/BlackFalcon1961/Luma3DS-FR-3GX-Loader/wiki/Remerciements)
+
+#
+### Licenses
+Ce logiciel est sous licence selon les termes de la GPLv3.  Vous pouvez trouver une copie de la licence dans le fichier `LICENSE.txt`.
+
+Les fichiers du stub GDB sont à la place sous licence triple en tant que MIT ou "GPLv2 ou toute version ultérieure", auquel cas il est spécifié dans l'en-tête du fichier.
---
+++ sysmodules/rosalina/source/menus/sysconfig.c
@@ -0,0 +1,380 @@
+/*
+*   This file is part of Luma3DS
+*   Copyright (C) 2016-2021 Aurora Wright, TuxSH
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+*       * Requiring preservation of specified reasonable legal notices or
+*         author attributions in that material or in the Appropriate Legal
+*         Notices displayed by works containing it.
+*       * Prohibiting misrepresentation of the origin of that material,
+*         or requiring that modified versions of such material be marked in
+*         reasonable ways as different from the original version.
+*/
+
+#include <3ds.h>
+#include "menus/sysconfig.h"
+#include "memory.h"
+#include "draw.h"
+#include "fmt.h"
+#include "utils.h"
+#include "ifile.h"
+
+Menu sysconfigMenu = {
+    "System configuration menu",
+    {
+        { "Control Wireless connection", METHOD, .method = &SysConfigMenu_ControlWifi },
+        { "Toggle LEDs", METHOD, .method = &SysConfigMenu_ToggleLEDs },
+        { "Toggle Wireless", METHOD, .method = &SysConfigMenu_ToggleWireless },
+        { "Toggle Power Button", METHOD, .method=&SysConfigMenu_TogglePowerButton },
+        { "Toggle power to card slot", METHOD, .method=&SysConfigMenu_ToggleCardIfPower},
+        {},
+    }
+};
+
+bool isConnectionForced = false;
+
+void SysConfigMenu_ToggleLEDs(void)
+{
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Press A to toggle, press B to go back.");
+        Draw_DrawString(10, 50, COLOR_RED, "WARNING:");
+        Draw_DrawString(10, 60, COLOR_WHITE, "  * Entering sleep mode will reset the LED state!");
+        Draw_DrawString(10, 70, COLOR_WHITE, "  * LEDs cannot be toggled when the battery is low!");
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+
+        u32 pressed = waitInputWithTimeout(1000);
+
+        if(pressed & KEY_A)
+        {
+            mcuHwcInit();
+            u8 result;
+            MCUHWC_ReadRegister(0x28, &result, 1);
+            result = ~result;
+            MCUHWC_WriteRegister(0x28, &result, 1);
+            mcuHwcExit();
+        }
+        else if(pressed & KEY_B)
+            return;
+    }
+    while(!menuShouldExit);
+}
+
+void SysConfigMenu_ToggleWireless(void)
+{
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    bool nwmRunning = isServiceUsable("nwm::EXT");
+
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Press A to toggle, press B to go back.");
+
+        u8 wireless = (*(vu8 *)((0x10140000 | (1u << 31)) + 0x180));
+
+        if(nwmRunning)
+        {
+            Draw_DrawString(10, 50, COLOR_WHITE, "Current status:");
+            Draw_DrawString(100, 50, (wireless ? COLOR_GREEN : COLOR_RED), (wireless ? " ON " : " OFF"));
+        }
+        else
+        {
+            Draw_DrawString(10, 50, COLOR_RED, "NWM isn't running.");
+            Draw_DrawString(10, 60, COLOR_RED, "If you're currently on Test Menu,");
+            Draw_DrawString(10, 70, COLOR_RED, "exit then press R+RIGHT to toggle the WiFi.");
+            Draw_DrawString(10, 80, COLOR_RED, "Otherwise, simply exit and wait a few seconds.");
+        }
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+
+        u32 pressed = waitInputWithTimeout(1000);
+
+        if(pressed & KEY_A && nwmRunning)
+        {
+            nwmExtInit();
+            NWMEXT_ControlWirelessEnabled(!wireless);
+            nwmExtExit();
+        }
+        else if(pressed & KEY_B)
+            return;
+    }
+    while(!menuShouldExit);
+}
+
+void SysConfigMenu_UpdateStatus(bool control)
+{
+    MenuItem *item = &sysconfigMenu.items[0];
+
+    if(control)
+    {
+        item->title = "Control Wireless connection";
+        item->method = &SysConfigMenu_ControlWifi;
+    }
+    else
+    {
+        item->title = "Disable forced wireless connection";
+        item->method = &SysConfigMenu_DisableForcedWifiConnection;
+    }
+}
+
+static bool SysConfigMenu_ForceWifiConnection(int slot)
+{
+    char ssid[0x20 + 1] = {0};
+    isConnectionForced = false;
+
+    if(R_FAILED(acInit()))
+        return false;
+
+    acuConfig config = {0};
+    ACU_CreateDefaultConfig(&config);
+    ACU_SetNetworkArea(&config, 2);
+    ACU_SetAllowApType(&config, 1 << slot);
+    ACU_SetRequestEulaVersion(&config);
+
+    Handle connectEvent = 0;
+    svcCreateEvent(&connectEvent, RESET_ONESHOT);
+
+    bool forcedConnection = false;
+    if(R_SUCCEEDED(ACU_ConnectAsync(&config, connectEvent)))
+    {
+        if(R_SUCCEEDED(svcWaitSynchronization(connectEvent, -1)) && R_SUCCEEDED(ACU_GetSSID(ssid)))
+            forcedConnection = true;
+    }
+    svcCloseHandle(connectEvent);
+
+    if(forcedConnection)
+    {
+        isConnectionForced = true;
+        SysConfigMenu_UpdateStatus(false);
+    }
+    else
+        acExit();
+
+    char infoString[80] = {0};
+    u32 infoStringColor = forcedConnection ? COLOR_GREEN : COLOR_RED;
+    if(forcedConnection)
+        sprintf(infoString, "Succesfully forced a connection to: %s", ssid);
+    else
+       sprintf(infoString, "Failed to connect to slot %d", slot + 1);
+
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        Draw_DrawString(10, 30, infoStringColor, infoString);
+        Draw_DrawString(10, 40, COLOR_WHITE, "Press B to go back.");
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+
+        u32 pressed = waitInputWithTimeout(1000);
+
+        if(pressed & KEY_B)
+            break;
+    }
+    while(!menuShouldExit);
+
+    return forcedConnection;
+}
+
+void SysConfigMenu_TogglePowerButton(void)
+{
+    u32 mcuIRQMask;
+
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    mcuHwcInit();
+    MCUHWC_ReadRegister(0x18, (u8*)&mcuIRQMask, 4);
+    mcuHwcExit();
+
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Press A to toggle, press B to go back.");
+
+        Draw_DrawString(10, 50, COLOR_WHITE, "Current status:");
+        Draw_DrawString(100, 50, (((mcuIRQMask & 0x00000001) == 0x00000001) ? COLOR_RED : COLOR_GREEN), (((mcuIRQMask & 0x00000001) == 0x00000001) ? " DISABLED" : " ENABLED "));
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+
+        u32 pressed = waitInputWithTimeout(1000);
+
+        if(pressed & KEY_A)
+        {
+            mcuHwcInit();
+            MCUHWC_ReadRegister(0x18, (u8*)&mcuIRQMask, 4);
+            mcuIRQMask ^= 0x00000001;
+            MCUHWC_WriteRegister(0x18, (u8*)&mcuIRQMask, 4);
+            mcuHwcExit();
+        }
+        else if(pressed & KEY_B)
+            return;
+    }
+    while(!menuShouldExit);
+}
+
+void SysConfigMenu_ControlWifi(void)
+{
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    int slot = 0;
+    char slotString[12] = {0};
+    sprintf(slotString, ">1<  2   3 ");
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Press A to force a connection to slot:");
+        Draw_DrawString(10, 40, COLOR_WHITE, slotString);
+        Draw_DrawString(10, 60, COLOR_WHITE, "Press B to go back.");
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+
+        u32 pressed = waitInputWithTimeout(1000);
+
+        if(pressed & KEY_A)
+        {
+            if(SysConfigMenu_ForceWifiConnection(slot))
+            {
+                // Connection successfully forced, return from this menu to prevent ac handle refcount leakage.
+                break;
+            }
+
+            Draw_Lock();
+            Draw_ClearFramebuffer();
+            Draw_FlushFramebuffer();
+            Draw_Unlock();
+        }
+        else if(pressed & KEY_LEFT)
+        {
+            slotString[slot * 4] = ' ';
+            slotString[(slot * 4) + 2] = ' ';
+            slot--;
+            if(slot == -1)
+                slot = 2;
+            slotString[slot * 4] = '>';
+            slotString[(slot * 4) + 2] = '<';
+        }
+        else if(pressed & KEY_RIGHT)
+        {
+            slotString[slot * 4] = ' ';
+            slotString[(slot * 4) + 2] = ' ';
+            slot++;
+            if(slot == 3)
+                slot = 0;
+            slotString[slot * 4] = '>';
+            slotString[(slot * 4) + 2] = '<';
+        }
+        else if(pressed & KEY_B)
+            return;
+    }
+    while(!menuShouldExit);
+}
+
+void SysConfigMenu_DisableForcedWifiConnection(void)
+{
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    acExit();
+    SysConfigMenu_UpdateStatus(true);
+
+    do
+    {
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        Draw_DrawString(10, 30, COLOR_WHITE, "Forced connection successfully disabled.\nNote: auto-connection may remain broken.");
+
+        u32 pressed = waitInputWithTimeout(1000);
+        if(pressed & KEY_B)
+            return;
+    }
+    while(!menuShouldExit);
+}
+
+void SysConfigMenu_ToggleCardIfPower(void)
+{
+    Draw_Lock();
+    Draw_ClearFramebuffer();
+    Draw_FlushFramebuffer();
+    Draw_Unlock();
+
+    bool cardIfStatus = false;
+    bool updatedCardIfStatus = false;
+
+    do
+    {
+        Result res = FSUSER_CardSlotGetCardIFPowerStatus(&cardIfStatus);
+        if (R_FAILED(res)) cardIfStatus = false;
+
+        Draw_Lock();
+        Draw_DrawString(10, 10, COLOR_TITLE, "System configuration menu");
+        u32 posY = Draw_DrawString(10, 30, COLOR_WHITE, "Press A to toggle, press B to go back.\n\n");
+        posY = Draw_DrawString(10, posY, COLOR_WHITE, "Inserting or removing a card will reset the status,\nand you'll need to reinsert the cart if you want to\nplay it.\n\n");
+        Draw_DrawString(10, posY, COLOR_WHITE, "Current status:");
+        Draw_DrawString(100, posY, !cardIfStatus ? COLOR_RED : COLOR_GREEN, !cardIfStatus ? " DISABLED" : " ENABLED ");
+
+        Draw_FlushFramebuffer();
+        Draw_Unlock();
+
+        u32 pressed = waitInputWithTimeout(1000);
+
+        if(pressed & KEY_A)
+        {
+            if (!cardIfStatus)
+                res = FSUSER_CardSlotPowerOn(&updatedCardIfStatus);
+            else
+                res = FSUSER_CardSlotPowerOff(&updatedCardIfStatus);
+
+            if (R_SUCCEEDED(res))
+                cardIfStatus = !updatedCardIfStatus;
+        }
+        else if(pressed & KEY_B)
+            return;
+    }
+    while(!menuShouldExit);
+}
